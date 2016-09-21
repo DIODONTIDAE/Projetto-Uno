a734c4b78f9831154dbcc887f34292da6c5424c1
DIODONTIDAE <zikeev.m@mail.ru>
2016-09-21 22:28:35 +0300

    Initial commit

---
+++ File-mngr/main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+
+char* cd(char *filename, char *added);
+void ls(char *filename);
+void clscr(void)
+{
+   printf("\033[2J");
+   printf("\033[0;0f");
+}
+
+int main()
+{
+    int mode=0;
+    char filename[256];
+    char tempname[20];
+    while (mode<4)
+    {
+        clscr();
+        scanf("%d",&mode);
+        switch (mode)
+        {
+        case 1:{clscr();ls(filename);break;}
+        case 2:{clscr();scanf("%s",tempname);strcat(tempname,"/");clscr();cd(filename,tempname);ls(filename);break;}
+        case 3:{clscr();strcat(filename,"/..");ls(filename);break;}
+        }
+    }
+    //ls(filename);
+    return 0;
+}
+char* cd(char* filename,char* added)
+{
+    strcat(filename,added);
+    return filename;
+}
+
+void ls(char* filename)
+{
+    DIR *dfd;
+    struct dirent *dp;
+
+    dfd=opendir(filename);
+    while( (dp=readdir(dfd)) != NULL )
+            printf("%10s.dir\n", dp->d_name);
+    closedir(dfd);
+}
